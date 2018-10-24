13b95ca1f070a48496d932c5cfe4bba02291670e
Flexpay <Flexpay@Flexpay-PC>
2018-10-24 16:54:22 +0100

    kitu

---
+++ Object Repository/Merchant Registration/Merchant registration using correct data.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>This is to test whether the merchant register without the merchant name but with other fields filled correctly</description>
+   <name>Merchant registration using correct data</name>
+   <tag></tag>
+   <elementGuidId>00994d9e-4afa-4358-9d08-96ca7ac98fd9</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;&quot;,
+  &quot;contentType&quot;: &quot;text/plain&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>http://176.58.103.227:8083/api/merchant_signup?username=bag&amp;email=bag@gmail.com&amp;password=bag1234&amp;confirm_password=bag1234&amp;merchant_name=bags&amp;agent_id=10</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/Merchant Registration.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Merchant Registration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-10-24T16:40:45</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8f8c89bd-7c9e-41e5-9c13-be4cc779797c</testSuiteGuid>
+   <testCaseLink>
+      <guid>d8b3ad78-6cf0-47cd-acac-138d722230af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Merchant Registration/validate if a merchant can successfully register</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
